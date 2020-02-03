73837363cdf6792582ef4733d03e7d3d6d0ea6f1
frosal <fernan735@gmail.com>
2020-02-03 17:26:09 +0100

    investigando Toolbar kotlin

--- app/src/main/java/com/ferrocu/eveniment/listadoAcont.kt
+++ app/src/main/java/com/ferrocu/eveniment/listadoAcont.kt
@@ -1,16 +1,26 @@
 package com.ferrocu.eveniment
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toolbar
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_acerca_de.*
 
 import kotlinx.android.synthetic.main.activity_listado_acont.*
+import kotlinx.android.synthetic.main.activity_listado_acont.fab
 
 class listadoAcont : AppCompatActivity() {
 
+    private var TAG:String="Listado"
+    private lateinit var context:Context
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        context=this
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_listado_acont)
+
+        val toolbar:Toolbar=findViewById(R.id.Apptoolbar)
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
@@ -19,4 +29,31 @@ class listadoAcont : AppCompatActivity() {
         }
     }
 
+    override fun onNavigateUp(): Boolean {
+        return true
+    }
+
+    override fun onBackPressed() {
+        finish()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
 }
